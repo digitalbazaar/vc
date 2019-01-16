42ddc6cbb444f35d5016e3a1c7b7c75c4995c7e5
Matthew Collier <collier.matthew@gmail.com>
2019-01-16 16:36:17 -0500

    Refactor document loaders.

---
+++ tests/MultiLoader.js
@@ -0,0 +1,32 @@
+class MultiLoader {
+  constructor({documentLoader} = {}) {
+    this.loaders = [];
+    if(documentLoader) {
+      this.loaders = this.loaders.concat(documentLoader);
+    }
+  }
+
+  addLoader(loader) {
+    this.loaders.push(loader);
+  }
+
+  async documentLoader(url) {
+    let result;
+    for(const loader of this.loaders) {
+      try {
+        result = await loader(url);
+      } catch(e) {
+        // this loader failed move on to the next
+        continue;
+      }
+      if(result) {
+        return result;
+      }
+    }
+    // failure, throw
+    throw new Error(`Document not found: ${url}`);
+  }
+
+} // end MultiLoader
+
+module.exports = MultiLoader;
