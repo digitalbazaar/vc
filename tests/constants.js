75f7ace0b286c6df5857d0bdf757421a0611259c
Dmitri Zagidulin <dzagidulin@gmail.com>
2019-04-11 13:12:51 -0400

    Pass rountrip issue / verify.

--- tests/constants.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  CREDENTIALS_CONTEXT_URL: 'https://w3id.org/credentials/v1',
-  DID_CONTEXT_URL: 'https://w3id.org/did/v0.11',
-  VERES_ONE_CONTEXT_URL: 'https://w3id.org/veres-one/v1'
-};
