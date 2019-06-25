ec265447cecdd7a243303340eb1791d10d039247
andrewjones <ajones@digitalbazaar.com>
2019-06-25 15:31:38 -0400

    Fix clone function.

--- tests/20-util.spec.js
+++ tests/20-util.spec.js
@@ -4,10 +4,18 @@ const {expect} = require('chai');
 describe('unit tests for util.js', function() {
   describe('clone', function() {
     it('should clone a number', function() {
-      const result = util.clone(5);
+      const value = 5;
+      const result = util.clone(value);
       expect(result).to.not.be.undefined;
       expect(result).to.not.be.null;
-      expect(result).to.equal(5);
+      expect(result).to.equal(value);
+    });
+    it('should clone a string', function() {
+      const value = 'string!';
+      const result = util.clone(value);
+      expect(result).to.not.be.undefined;
+      expect(result).to.not.be.null;
+      expect(result).to.equal(value);
     });
     it('should clone an Object', function() {
       const obj = {num: 5, bool: false, func: () => 1};
@@ -15,6 +23,15 @@ describe('unit tests for util.js', function() {
       expect(result).to.not.be.undefined;
       expect(result).to.not.be.null;
       expect(result).to.deep.equal(obj);
+      expect(obj === result, 'Expected different references').to.be.false;
+    });
+    it('should clone an Array', function() {
+      const obj = [5, 'cloned', true, {deep: {deep: 'deeper!'}}];
+      const result = util.clone(obj);
+      expect(result).to.not.be.undefined;
+      expect(result).to.not.be.null;
+      expect(result).to.deep.equal(obj);
+      expect(obj === result, 'Expected different references').to.be.false;
     });
   });
 });
