c44fd593ec288952bc45a34c8438db008066bdce
Dmitri Zagidulin <dzagidulin@gmail.com>
2019-08-23 13:41:08 -0400

    Exclude bitcore-lib in webpack bundle.

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,9 @@ module.exports = {
   entry: {
     'vc-js': './lib/index.js'
   },
+  externals: {
+    'bitcore-lib': '\'bitcore-lib\''
+  },
   output: {
     filename: '[name].min.js',
     library: 'VC',
