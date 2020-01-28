a3a9e2641684793ed2cc8ebc166e6ffd037fac2e
Dmitri Zagidulin <dzagidulin@gmail.com>
2020-01-28 16:26:54 -0500

    Check that vc issuer matches proof controller.

--- tests/10-verify.spec.js
+++ tests/10-verify.spec.js
@@ -52,7 +52,7 @@ async function documentLoader(url) {
 
 const assertionController = {
   '@context': 'https://w3id.org/security/v2',
-  id: 'https://example.com/i/carol',
+  id: 'https://example.edu/issuers/565049',
   assertionMethod: [
     // actual key is going to be added in the before() block below
   ]
@@ -141,6 +141,7 @@ describe('verify()', () => {
       suite,
       documentLoader
     });
+
     if(result.error) {
       throw result.error;
     }
