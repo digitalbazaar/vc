e4294b733b1cadb4f41850e46d7c582c903a8e87
Andrew Jones <ajones@digitalbazaar.com>
2020-01-29 13:01:04 -0500

    Update assertionMethod so it matches the issuer.

--- tests/mocks/assertionController.js
+++ tests/mocks/assertionController.js
@@ -1,6 +1,6 @@
 const assertionController = {
   '@context': 'https://w3id.org/security/v2',
-  id: 'https://example.com/i/carol',
+  id: 'https://example.edu/issuers/565049',
   assertionMethod: [
     // actual key is going to be added in the before() block below
   ]
