fe39c2ab1fabbb3e167fbca89d9bbc4e4caa5e1e
Dmitri Zagidulin <dzagidulin@gmail.com>
2020-02-03 12:47:56 -0500

    Add unit tests for createPresentation and signPresentation.

--- tests/mocks/assertionController.js
+++ tests/mocks/assertionController.js
@@ -1,8 +1,8 @@
 const assertionController = {
   '@context': 'https://w3id.org/security/v2',
   id: 'https://example.edu/issuers/565049',
-  assertionMethod: [
-    // actual key is going to be added in the before() block below
-  ]
+  // actual keys are going to be added in the test suite before() block
+  assertionMethod: [],
+  authentication: []
 };
 module.exports = assertionController;
