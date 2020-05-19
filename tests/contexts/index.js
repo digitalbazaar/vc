d6663a7af70440ebd4e0484d7d3d0d0f303ec7ec
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2020-05-19 10:15:01 -0400

    Use did-context in tests.

--- tests/contexts/index.js
+++ tests/contexts/index.js
@@ -1,4 +1,6 @@
 const credentialsContext = require('credentials-context');
+const didContext = require('did-context');
+
 const invalidContexts = {
   veresOne: {
     url: 'https://w3id.org/veres-one/v1',
@@ -6,11 +8,13 @@ const invalidContexts = {
   },
   did: {
     url: 'https://w3id.org/did/v0.11',
-    value: require('./didv0.json')
+    value: didContext.contexts.get('https://w3id.org/did/v0.11')
   },
   valid: {
     url: 'https://www.w3.org/2018/credentials/v1',
-    value: credentialsContext.contexts.get('https://www.w3.org/2018/credentials/v1')
+    value: credentialsContext.contexts.get(
+      'https://www.w3.org/2018/credentials/v1'
+    )
   },
   invalidId: {
     url: 'https://invalid-id.org',
