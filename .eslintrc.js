3d4754bcf35245be04f18fd1b33a28f4698cde5e
Dmitri Zagidulin <dzagidulin@gmail.com>
2021-03-17 22:39:03 -0400

    Set up lint, fix mocha script.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,22 @@
+/*!
+ * Copyright (c) 2021 Digital Bazaar, Inc. All rights reserved.
+ */
 module.exports = {
   root: true,
+  extends: [
+    'eslint-config-digitalbazaar',
+    // 'eslint-config-digitalbazaar/jsdoc'
+  ],
   env: {
     node: true
   },
-  extends: [
-    'digitalbazaar',
-    'digitalbazaar/jsdoc'
-  ],
-  ignorePatterns: ['dist/']
+  parserOptions: {
+    // this is required for dynamic import()
+    ecmaVersion: 2020
+  },
+  ignorePatterns: ['node_modules', 'dist'],
+  rules: {
+    'jsdoc/check-examples': 0,
+    'jsdoc/require-description-complete-sentence': 0
+  }
 };
