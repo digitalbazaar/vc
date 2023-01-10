40e0643122a42660f3f701e358b8ceca00545cdc
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2023-01-10 11:18:59 -0500

    Update copyright year.

--- test/10-verify.spec.js
+++ test/10-verify.spec.js
@@ -1,5 +1,5 @@
 /*!
- * Copyright (c) 2019-2022 Digital Bazaar, Inc. All rights reserved.
+ * Copyright (c) 2019-2023 Digital Bazaar, Inc. All rights reserved.
  */
 import chai from 'chai';
 const should = chai.should();
