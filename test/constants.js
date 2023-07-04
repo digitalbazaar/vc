f671efb41498155ddac89fe958f4f694f0c34e15
Andrew Jones <ajones@digitalbazaar.com>
2023-07-04 20:06:53 +0000

    Make VC v2 context default.

--- test/constants.js
+++ test/constants.js
@@ -1,5 +1,6 @@
 export default {
-  CREDENTIALS_CONTEXT_URL: 'https://www.w3.org/2018/credentials/v1',
+  CREDENTIALS_CONTEXT_URL: 'https://www.w3.org/ns/credentials/v2',
+  CREDENTIALS_CONTEXT_V1_URL: 'https://www.w3.org/2018/credentials/v1',
   DID_CONTEXT_URL: 'https://www.w3.org/ns/did/v1',
   VERES_ONE_CONTEXT_URL: 'https://w3id.org/veres-one/v1'
 };
