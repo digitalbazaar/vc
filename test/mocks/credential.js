d2027286d09cf5c8b8491ca601b2f7ea334b13af
Andrew Jones <ajones@digitalbazaar.com>
2023-07-04 20:06:53 +0000

    Make VC v2 context default.

--- test/mocks/credential.js
+++ test/mocks/credential.js
@@ -1,6 +1,6 @@
 export const credential = {
   '@context': [
-    'https://www.w3.org/2018/credentials/v1',
+    'https://www.w3.org/ns/credentials/v2',
     'https://www.w3.org/2018/credentials/examples/v1'
   ],
   id: 'http://example.edu/credentials/1872',
