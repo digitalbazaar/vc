8c8af9cc8948bf4c895244354f2fef5924bcb063
Andrew Jones <ajones@digitalbazaar.com>
2023-07-06 15:53:02 +0000

    Add v1 and v2 mock credential.

--- test/mocks/credential.js
+++ test/mocks/credential.js
@@ -11,3 +11,33 @@ export const credential = {
     alumniOf: '<span lang="en">Example University</span>'
   }
 };
+
+export const credentials = {
+  v1: {
+    '@context': [
+      'https://www.w3.org/2018/credentials/v1',
+      'https://www.w3.org/2018/credentials/examples/v1'
+    ],
+    id: 'http://example.edu/credentials/1872',
+    type: ['VerifiableCredential', 'AlumniCredential'],
+    issuer: 'https://example.edu/issuers/565049',
+    issuanceDate: '2010-01-01T19:23:24Z',
+    credentialSubject: {
+      id: 'did:example:ebfeb1f712ebc6f1c276e12ec21',
+      alumniOf: '<span lang="en">Example University</span>'
+    }
+  },
+  v2: {
+    '@context': [
+      'https://www.w3.org/ns/credentials/v2',
+      'https://www.w3.org/2018/credentials/examples/v1'
+    ],
+    id: 'http://example.edu/credentials/1872',
+    type: ['VerifiableCredential', 'AlumniCredential'],
+    issuer: 'https://example.edu/issuers/565049',
+    credentialSubject: {
+      id: 'did:example:ebfeb1f712ebc6f1c276e12ec21',
+      alumniOf: '<span lang="en">Example University</span>'
+    }
+  }
+};
