6ff3e53ea0334e6d0e11ef51be4c8fc2065a77a0
Andrew Jones <ajones@digitalbazaar.com>
2023-07-10 15:42:57 +0000

    Start moving test data to use both v1 and v2 credentials.

--- test/10-verify.spec.js
+++ test/10-verify.spec.js
@@ -13,6 +13,7 @@ import {
 import {assertionController} from './mocks/assertionController.js';
 import chai from 'chai';
 import {CredentialIssuancePurpose} from '../lib/CredentialIssuancePurpose.js';
+import {credentials} from './mocks/credential.js';
 import dataIntegrityContext from '@digitalbazaar/data-integrity-context';
 import {DataIntegrityProof} from '@digitalbazaar/data-integrity';
 import {Ed25519Signature2018} from '@digitalbazaar/ed25519-signature-2018';
@@ -22,7 +23,6 @@ import {
 import {invalidContexts} from './contexts/index.js';
 import jsigs from 'jsonld-signatures';
 import jsonld from 'jsonld';
-import {credential as mockCredential} from './mocks/credential.js';
 import {mock as mockData} from './mocks/mock.data.js';
 import multikeyContext from '@digitalbazaar/multikey-context';
 import {v4 as uuid} from 'uuid';
@@ -100,7 +100,7 @@ before(async () => {
 
 describe('vc.issue()', () => {
   it('should issue a verifiable credential with proof', async () => {
-    const credential = jsonld.clone(mockCredential);
+    const credential = jsonld.clone(credentials.v2);
     const verifiableCredential = await vc.issue({
       credential,
       suite,
@@ -117,7 +117,7 @@ describe('vc.issue()', () => {
     const fp = Ed25519VerificationKey2018
       .fingerprintFromPublicKey({publicKeyBase58: keyPair.publicKeyBase58});
     keyPair.id = `did:key:${fp}#${fp}`;
-    const credential = jsonld.clone(mockCredential);
+    const credential = jsonld.clone(credentials.v1);
     credential.id = `urn:uuid:${uuid()}`;
     credential.issuer = `did:key:${fp}`;
     credential.expirationDate = '2020-05-31T19:21:25Z';
@@ -145,7 +145,7 @@ describe('vc.issue()', () => {
     let error;
     try {
       await vc.issue({
-        credential: mockCredential,
+        credential: credentials.v2,
         suite
       });
     } catch(e) {
@@ -163,7 +163,7 @@ describe('vc.issue()', () => {
 describe('vc.createPresentation()', () => {
   it('should create an unsigned presentation', () => {
     const presentation = vc.createPresentation({
-      verifiableCredential: mockCredential,
+      verifiableCredential: credentials.v2,
       id: 'test:ebc6f1c2',
       holder: 'did:ex:holder123'
     });
@@ -179,7 +179,7 @@ describe('vc.createPresentation()', () => {
 describe('vc.signPresentation()', () => {
   it('should create a signed VP', async () => {
     const presentation = vc.createPresentation({
-      verifiableCredential: mockCredential,
+      verifiableCredential: credentials.v2,
       id: 'test:ebc6f1c2',
       holder: 'did:ex:holder123'
     });
@@ -204,7 +204,7 @@ describe('vc.signPresentation()', () => {
 describe('verify API (credentials)', () => {
   it('should verify a vc', async () => {
     const verifiableCredential = await vc.issue({
-      credential: mockCredential,
+      credential: credentials.v2,
       suite,
       documentLoader
     });
@@ -248,7 +248,7 @@ describe('verify API (credentials)', () => {
     });
 
     const verifiableCredential = await vc.issue({
-      credential: {...mockCredential},
+      credential: {...credentials.v1},
       suite: ecdsaSdSignSuite,
       documentLoader
     });
@@ -272,7 +272,7 @@ describe('verify API (credentials)', () => {
 
   it('should verify a vc with a positive status check', async () => {
     const verifiableCredential = await vc.issue({
-      credential: mockCredential,
+      credential: credentials.v2,
       suite,
       documentLoader
     });
@@ -370,7 +370,7 @@ describe('verify API (credentials)', () => {
     });
     it('should fail to verify a vc with a negative status check', async () => {
       const verifiableCredential = await vc.issue({
-        credential: mockCredential,
+        credential: credentials.v2,
         suite,
         documentLoader
       });
@@ -414,7 +414,7 @@ describe('verify API (credentials)', () => {
       });
 
       const verifiableCredential = await vc.issue({
-        credential: {...mockCredential},
+        credential: {...credentials.v1},
         suite: ecdsaSdSignSuite,
         documentLoader
       });
