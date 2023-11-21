ed8926471776491517038961139d771d77b22169
Andrew Jones <ajones@digitalbazaar.com>
2023-11-21 23:34:15 +0000

    Tests run on both v1 and v2 VCs.

--- test/mocks/credential.js
+++ test/mocks/credential.js
@@ -27,3 +27,33 @@ export const credentials = {
     }
   }
 };
+
+export const versionedCredentials = new Map([
+  ['1.0', {
+    '@context': [
+      'https://www.w3.org/2018/credentials/v1',
+      'https://www.w3.org/2018/credentials/examples/v1'
+    ],
+    id: 'http://example.edu/credentials/1872',
+    type: ['VerifiableCredential', 'AlumniCredential'],
+    issuer: 'https://example.edu/issuers/565049',
+    issuanceDate: '2010-01-01T19:23:24Z',
+    credentialSubject: {
+      id: 'did:example:ebfeb1f712ebc6f1c276e12ec21',
+      alumniOf: '<span lang="en">Example University</span>'
+    }
+  }],
+  ['2.0', {
+    '@context': [
+      'https://www.w3.org/ns/credentials/v2',
+      'https://www.w3.org/2018/credentials/examples/v1'
+    ],
+    id: 'http://example.edu/credentials/1872',
+    type: ['VerifiableCredential', 'AlumniCredential'],
+    issuer: 'https://example.edu/issuers/565049',
+    credentialSubject: {
+      id: 'did:example:ebfeb1f712ebc6f1c276e12ec21',
+      alumniOf: '<span lang="en">Example University</span>'
+    }
+  }]
+]);
