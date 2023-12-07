fdc98f6ce3e8238b37009b8baa7dcce1b2b2bb9d
Andrew Jones <ajones@digitalbazaar.com>
2023-12-07 22:12:19 +0000

    Use klona over jsonld.clone in all test data.

--- test/10-verify.spec.js
+++ test/10-verify.spec.js
@@ -20,7 +20,7 @@ import {
 } from '@digitalbazaar/ed25519-verification-key-2018';
 import {invalidContexts} from './contexts/index.js';
 import jsigs from 'jsonld-signatures';
-import jsonld from 'jsonld';
+import {klona} from 'klona';
 import {mock as mockData} from './mocks/mock.data.js';
 import {v4 as uuid} from 'uuid';
 import {VeresOneDriver} from 'did-veres-one';
@@ -629,7 +629,7 @@ for(const [version, mockCredential] of versionedCredentials) {
 
     describe('_checkCredential', () => {
       it('should reject a credentialSubject.id that is not a URI', () => {
-        const credential = jsonld.clone(mockData.credentials.alpha);
+        const credential = klona(mockData.credentials.alpha);
         credential.issuer = 'http://example.edu/credentials/58473';
         credential.credentialSubject.id = '12345';
         let error;
@@ -646,7 +646,7 @@ for(const [version, mockCredential] of versionedCredentials) {
       });
 
       it('should reject an issuer that is not a URI', () => {
-        const credential = jsonld.clone(mockData.credentials.alpha);
+        const credential = klona(mockData.credentials.alpha);
         credential.issuer = '12345';
         let error;
         try {
@@ -662,7 +662,7 @@ for(const [version, mockCredential] of versionedCredentials) {
       });
 
       it('should reject an evidence id that is not a URI', () => {
-        const credential = jsonld.clone(mockData.credentials.alpha);
+        const credential = klona(mockData.credentials.alpha);
         credential.issuer = 'did:example:12345';
         credential.evidence = '12345';
         let error;
@@ -679,7 +679,7 @@ for(const [version, mockCredential] of versionedCredentials) {
       });
 
       it('should reject if "expirationDate" has passed', () => {
-        const credential = jsonld.clone(mockData.credentials.alpha);
+        const credential = klona(mockData.credentials.alpha);
         credential.issuer = 'did:example:12345';
         // set expirationDate to an expired date.
         credential.expirationDate = '2020-05-31T19:21:25Z';
