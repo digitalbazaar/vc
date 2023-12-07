b7b9571da0a909e65244944b611aa50c6882f86f
Andrew Jones <ajones@digitalbazaar.com>
2023-12-07 21:14:13 +0000

    Use klona for test data & move klona to mock file.

--- test/mocks/credential.js
+++ test/mocks/credential.js
@@ -1,5 +1,10 @@
+/*!
+* Copyright (c) 2019-2023 Digital Bazaar, Inc. All rights reserved.
+*/
+import {klona} from 'klona';
+
 export const versionedCredentials = new Map([
-  [1.0, {
+  [1.0, () => klona({
     '@context': [
       'https://www.w3.org/2018/credentials/v1',
       'https://www.w3.org/2018/credentials/examples/v1'
@@ -12,8 +17,8 @@ export const versionedCredentials = new Map([
       id: 'did:example:ebfeb1f712ebc6f1c276e12ec21',
       alumniOf: '<span lang="en">Example University</span>'
     }
-  }],
-  [2.0, {
+  })],
+  [2.0, () => klona({
     '@context': [
       'https://www.w3.org/ns/credentials/v2',
       'https://www.w3.org/2018/credentials/examples/v1'
@@ -25,5 +30,5 @@ export const versionedCredentials = new Map([
       id: 'did:example:ebfeb1f712ebc6f1c276e12ec21',
       alumniOf: '<span lang="en">Example University</span>'
     }
-  }]
+  })]
 ]);
