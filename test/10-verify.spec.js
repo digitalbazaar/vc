618227b699b64dd905bf77e85f2811aa09842fd1
Andrew Jones <ajones@digitalbazaar.com>
2024-01-02 17:53:15 +0000

    Update test/10-verify.spec.js remove extra blank line.
    Co-authored-by: Tashi D. Gyeltshen <38434620+JSAssassin@users.noreply.github.com>

--- test/10-verify.spec.js
+++ test/10-verify.spec.js
@@ -113,7 +113,6 @@ for(const [version, mockCredential] of versionedCredentials) {
         } catch(e) {
           error = e;
         }
-
         should.exist(error,
           'Should throw error when "verificationMethod" property is missing');
         error.should.be.instanceof(TypeError);
