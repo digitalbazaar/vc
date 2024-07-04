90027919173cc4318352c3b1862386ef5ae17e2f
Andrew Jones <ajones@digitalbazaar.com>
2024-07-04 22:01:01 +0000

    Remove unusued CREDENTIALS_CONTEXT_URL from test constants.

--- test/constants.js
+++ test/constants.js
@@ -1,5 +1,4 @@
 export default {
-  CREDENTIALS_CONTEXT_URL: 'https://www.w3.org/ns/credentials/v2',
   CREDENTIALS_CONTEXT_V1_URL: 'https://www.w3.org/2018/credentials/v1',
   CREDENTIALS_CONTEXT_V2_URL: 'https://www.w3.org/ns/credentials/v2',
   DID_CONTEXT_URL: 'https://www.w3.org/ns/did/v1',
