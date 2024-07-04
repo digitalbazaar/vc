c8f4007ed7f97c6c170ef032a57e80abaeafc58a
Andrew Jones <ajones@digitalbazaar.com>
2024-07-04 21:57:10 +0000

    Add Credentials Contet V2 URL to test constants.

--- test/constants.js
+++ test/constants.js
@@ -1,6 +1,7 @@
 export default {
   CREDENTIALS_CONTEXT_URL: 'https://www.w3.org/ns/credentials/v2',
   CREDENTIALS_CONTEXT_V1_URL: 'https://www.w3.org/2018/credentials/v1',
+  CREDENTIALS_CONTEXT_V2_URL: 'https://www.w3.org/ns/credentials/v2',
   DID_CONTEXT_URL: 'https://www.w3.org/ns/did/v1',
   VERES_ONE_CONTEXT_URL: 'https://w3id.org/veres-one/v1'
 };
