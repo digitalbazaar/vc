8eb46db34a22d96df87d80696586682bfae0c9a4
Andrew Jones <ajones@digitalbazaar.com>
2024-07-21 12:26:19 -0400

    Move generateCredential to helpers.

--- test/helpers.js
+++ test/helpers.js
@@ -1,3 +1,13 @@
+/*!
+ * Copyright (c) 2019-2024 Digital Bazaar, Inc. All rights reserved.
+ */
+import {CredentialIssuancePurpose} from '../lib/CredentialIssuancePurpose.js';
+import {
+  documentLoader as defaultLoader
+} from './testDocumentLoader.js';
+import jsigs from 'jsonld-signatures';
+import {v4 as uuid} from 'uuid';
+
 /**
  * Creates an ISO DateTime skewed by a number of years
  *
@@ -11,4 +21,22 @@ export function createSkewedTimeStamp({date = new Date(), skewYear}) {
   date.setFullYear(date.getFullYear() + skewYear);
   const isoString = date.toISOString();
   return `${isoString.substring(0, isoString.length - 5)}Z`;
+}
+
+export async function generateCredential({
+  credentialFactory,
+  suite,
+  issuer,
+  documentLoader = defaultLoader
+}) {
+  const mockCredential = credentialFactory();
+  mockCredential.issuer = issuer;
+  mockCredential.id = `http://example.edu/credentials/${uuid()}`;
+  const credential = await jsigs.sign(mockCredential, {
+    compactProof: false,
+    documentLoader,
+    suite,
+    purpose: new CredentialIssuancePurpose()
+  });
+  return {credential};
 }
